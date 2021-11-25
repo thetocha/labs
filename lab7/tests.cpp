97e1a62cd73508bbada8d144bf93892d79431b47
Anton Hubko <antongub3@gmail.com>
2021-11-25 16:13:57 +0300

    finished gtests, in my opinion

--- lab7/tests.cpp
+++ lab7/tests.cpp
@@ -34,5 +34,17 @@ TEST(AverengeScore_from_10_8_9_6_7_8_9_8_10_FOR_2ndTerm,8_11){
     int grades[4]={10, 8, 9, 6};
     int grades1[5]={7, 8, 9, 6, 10};
     StudentAfter2ndTerm studentAfter2NdTerm("Anton", 2, 3, 34, grades, grades1);
-    ASSERT_NEAR(studentAfter2NdTerm.AverengeScore(),8.11,0.0111111);
+    ASSERT_NEAR(studentAfter2NdTerm.AverengeScore(),8.11,0.01);
+}
+
+
+TEST(AverageScore_For_1stTerm_grades_10_8_9_6, result_8_25){
+    int grades[4]={10, 8, 9, 6};
+    StudentAfter1stTerm stTerm("Anton", 2, 3, 34, grades);
+    ASSERT_DOUBLE_EQ(stTerm.AverengeScore(),8.25);
+}
+
+TEST(AverageScore_for_Student,result_0){
+    Student student("Anton", 2, 3, 34);
+    ASSERT_EQ(student.AverengeScore(),0);
 }
