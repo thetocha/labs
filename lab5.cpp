02098f9af2ba00ebe84633a36ce8d9a46db2ebe5
Anton Hubko <antongub3@gmail.com>
2021-12-09 11:38:44 +0300

    remade lab5

--- lab5.cpp
+++ lab5.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+
+int DefenitionOfProgerssion(string string1){
+    int progerssion = 0;
+    if(string1 == "")
+        return 0;
+    else{
+        for (int j = 0; j < string1.size()-1; ++j){
+            if(string1[j] < string1[j+1])
+                ++progerssion;
+            else if (string1[j] > string1[j+1])
+                --progerssion;
+            else
+                continue;
+        }
+    }
+    if(progerssion == string1.size() - 1)
+        return 1;
+    else if(progerssion == -(string1.size() - 1))
+        return -1;
+    else
+        return 0;
+}
+
 int main() {
     ofstream fout;
     fout.open("Newfile.txt");
@@ -11,8 +34,9 @@ int main() {
         cout << "The file isn't open";
     } else{
         fout << "abcd" << endl;
+        fout << "aoyz" << endl;
         fout << "dcba" << endl;
-        fout << "dacb" << endl;
+        fout << "dacb" << endl; // ?
     }
     fout.close();
 
@@ -23,55 +47,53 @@ int main() {
     if (!fin.is_open()){
         cout << "The file isn't open";
     } else{
-        for (int i = 0; i < 3; ++i) {
+        while(!fin.eof()) {
             getline(fin, str);
-            cout << str << endl;
-            for (int j = 0; j < str.size()-1; ++j) {
-                if(str[j] < str[j+1]){
-                    ofstream increase;
-                    increase.open("Increase_strings.txt");
-                    if (!increase.is_open()){
-                        cout << "The file isn't open";
-                    } else{
-                        increase << str;
-                    }
-                    increase.close();
-                }else if (str[j] > str[j+1]){
-                    ofstream decrease;
-                    decrease.open("Decrease_strings.txt");
-                    if (!decrease.is_open()){
-                        cout << "The file isn't open";
-                    } else{
-                        decrease << str;
-                    }
-                    decrease.close();
+            int a = DefenitionOfProgerssion(str);
+            if (a == 1){
+                ofstream fout;
+                fout.open("Increase_strings.txt", ios::app);
+                if (!fout.is_open()){
+                    cout << "The file isn't open" << endl;
                 }else{
-                    continue;
+                    fout << str << endl;
                 }
-
-            }
+            }else if (a == -1){
+                ofstream fout;
+                fout.open("Decrease_strings.txt", ios::app);
+                if (!fout.is_open()){
+                    cout << "The file isn't open" << endl;
+                }else{
+                    fout << str << endl;
+                }
+            }else
+                continue;
         }
     }
     fin.close();
 
     ifstream IncreaseReading("Increase_strings.txt");
     if(!IncreaseReading.is_open()){
-        cout << "File of increase strings is not open 33" << endl;
+        cout << "File of increase strings is not open " << endl;
     }else{
-        string string1;
-        getline(IncreaseReading,string1);
-        cout << string1 << endl;
+        while(!IncreaseReading.eof()){
+            string string1;
+            getline(IncreaseReading,string1);
+            cout << string1 << endl;
+        }
     }
     IncreaseReading.close();
 
-    ifstream DeccreaseReading;
-    DeccreaseReading.open("Decrease_strings.txt");
-    if(!DeccreaseReading.is_open()){
+    ifstream DecreaseReading;
+    DecreaseReading.open("Decrease_strings.txt");
+    if(!DecreaseReading.is_open()){
         cout << "File of decrease strings is not open" << endl;
     }else{
-        string string1;
-        getline(DeccreaseReading, string1);
-        cout << string1 << endl;
+        while(!DecreaseReading.eof()){
+            string string1;
+            getline(DecreaseReading, string1);
+            cout << string1 << endl;
+        }
     }
     IncreaseReading.close();
 
